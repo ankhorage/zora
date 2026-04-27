534876467457c606518d23a4c8b40bfd8b9132b0
artiphishle <fabio.gartenmann@protonmail.com>
2026-04-27 04:19:37 +0200

    Sort imports

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,6 @@
 export type { ZoraThemeOverride } from './createZoraTheme';
 export { createZoraTheme } from './createZoraTheme';
+export { useZoraTheme } from './useZoraTheme';
 export type { ZoraProviderProps } from './ZoraProvider';
 export { ZoraProvider } from './ZoraProvider';
 export { zoraTheme } from './zoraTheme';
-export { useZoraTheme } from './useZoraTheme';
