fc5f91502003901ac3dc62f7f7151f6d142d471e
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-10 19:42:32 +0200

    fix: keep input helper props private

--- src/components/input/types.ts
+++ src/components/input/types.ts
@@ -19,7 +19,7 @@ type InputTrailingProps =
       trailingAction?: InputTrailingAction;
     };
 
-export interface InputBaseProps
+interface InputBaseProps
   extends
     ZoraBaseProps,
     Omit<
