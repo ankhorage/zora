8e09bc8dfd7646b8af936e93b38dfd2664499c24
artiphishle <fabio.gartenmann@protonmail.com>
2026-05-16 19:49:39 +0200

    chore(lint): fix

--- src/patterns/post-card/index.ts
+++ src/patterns/post-card/index.ts
@@ -1,2 +1,8 @@
 export { PostCard } from './PostCard';
-export type { PostAction, PostAuthor, PostAuthorAvatar, PostCardMedia, PostCardProps } from './types';
+export type {
+  PostAction,
+  PostAuthor,
+  PostAuthorAvatar,
+  PostCardMedia,
+  PostCardProps,
+} from './types';
