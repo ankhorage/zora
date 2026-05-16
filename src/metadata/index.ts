8302f7711a11fa9cfd8309c133a8110c13a7ac62
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-16 11:15:13 +0200

    feat(metadata): export event metadata types

--- src/metadata/index.ts
+++ src/metadata/index.ts
@@ -2,6 +2,10 @@ export { ZORA_COMPONENT_META } from './componentMeta';
 export type {
   ZoraComponentBlueprint,
   ZoraComponentCategory,
+  ZoraComponentEventMeta,
+  ZoraComponentEventPayloadFieldMeta,
+  ZoraComponentEventPayloadFieldType,
+  ZoraComponentEventPayloadKind,
   ZoraComponentI18nMeta,
   ZoraComponentMeta,
   ZoraComponentMetaRegistry,
