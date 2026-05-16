ef4268bc15046c597d5763ee4ba50f780a374c60
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-05-16 22:58:37 +0200

    fix: avoid undefined toast duration prop

--- src/components/toast/ToastProvider.tsx
+++ src/components/toast/ToastProvider.tsx
@@ -4,5 +4,9 @@ import React from 'react';
 import type { ToastProviderProps } from './types';
 
 export function ToastProvider({ children, defaultDuration }: ToastProviderProps) {
+  if (defaultDuration === undefined) {
+    return <SurfaceToastProvider>{children}</SurfaceToastProvider>;
+  }
+
   return <SurfaceToastProvider defaultDuration={defaultDuration}>{children}</SurfaceToastProvider>;
 }
