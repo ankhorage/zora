019bfac0680404a01cd6dd8a3fd17ea806754426
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-06-09 01:06:58 +0200

    Fix card header press props

--- src/components/card/types.ts
+++ src/components/card/types.ts
@@ -6,4 +6,7 @@ import type { ZoraBaseProps } from '../../theme/ZoraBaseProps';
 
 export interface CardProps
   extends ZoraBaseProps, Omit<SurfaceCardProps, 'children' | 'p' | 'radius' | 'variant'> {
-  children?: React.ReactNode
+  children?: React.ReactNode;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  eyebrow
