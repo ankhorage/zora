a73f73b44d752de4cbfa51f8e6bd19b58c1b748f
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-08 17:02:27 +0200

    feat(radio): register group theme recipe

--- src/metadata/themeRecipeMeta.ts
+++ src/metadata/themeRecipeMeta.ts
@@ -1,6 +1,7 @@
 import { buttonThemeRecipeMeta } from '../components/button/themeRecipeMeta';
 import { cardThemeRecipeMeta } from '../components/card/themeRecipeMeta';
 import { headingThemeRecipeMeta } from '../components/heading/themeRecipeMeta';
+import { radioGroupThemeRecipeMeta } from '../components/radio/themeRecipeMeta';
 import { textThemeRecipeMeta } from '../components/text/themeRecipeMeta';
 import { panelThemeRecipeMeta } from '../patterns/panel/themeRecipeMeta';
 import type { ZoraThemeRecipeMetaRegistry } from './themeRecipeTypes';
@@ -9,6 +10,7 @@ export const ZORA_THEME_RECIPE_META: ZoraThemeRecipeMetaRegistry = {
   Button: buttonThemeRecipeMeta,
   Card: cardThemeRecipeMeta,
   Heading: headingThemeRecipeMeta,
+  RadioGroup: radioGroupThemeRecipeMeta,
   Text: textThemeRecipeMeta,
   Panel: panelThemeRecipeMeta,
 };
