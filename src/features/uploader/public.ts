38ab299208e08a03a8d84cc59ea4b9838804511b
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-10 01:42:53 +0200

    feat: expose generic Uploader

---
+++ src/features/uploader/public.ts
@@ -0,0 +1,8 @@
+export { Uploader } from './adapters/inbound/Uploader';
+export type {
+  UploadAsset,
+  UploadAssetBase,
+  UploadProgressContext,
+  UploadType,
+  UploaderProps,
+} from '../../types/upload';
