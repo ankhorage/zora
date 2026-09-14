e8ef67d74f31013c2ac0345b25c64704c137e3d6
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-14 16:32:48 +0200

    refactor: remove legacy toast barrel

--- src/components/toast/index.ts
+++
@@ -1,4 +0,0 @@
-export { Toast } from './Toast';
-export { ToastProvider } from './ToastProvider';
-export type { ToastOptions, ToastProps, ToastProviderProps, ToastStatus } from './types';
-export { useToast } from '@ankhorage/surface';
