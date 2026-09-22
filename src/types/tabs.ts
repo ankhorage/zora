4da67ed55d3b99f1adfedbda4845a9f23d415451
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-22 18:08:37 +0200

    style(tabs): format surface-derived props

--- src/types/tabs.ts
+++ src/types/tabs.ts
@@ -9,12 +9,10 @@ import type { ZoraBaseProps } from './base';
 
 export interface TabsProps extends ZoraBaseProps, Omit<SurfaceTabsProps, 'testID'> {}
 
-export interface TabListProps
-  extends ZoraBaseProps, Omit<SurfaceTabListProps, 'testID'> {}
+export interface TabListProps extends ZoraBaseProps, Omit<SurfaceTabListProps, 'testID'> {}
 
 export interface TabProps
-  extends
-    ZoraBaseProps,
+  extends ZoraBaseProps,
     Omit<SurfaceTabProps, 'children' | 'interactionPolicy' | 'testID'> {
   label: string;
 }
